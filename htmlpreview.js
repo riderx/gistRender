933d65e1401c2ee0b67ab5a68003cbc43462ad65
Martin DONADIEU <martindonadieu@gmail.com>
2018-02-28 23:21:32 +0100

    try to make relative files works <3
    remove my code to try it without
    
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Update htmlpreview.js
    
    fix error
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    make it work for gist
    
    rm old useless code
    
    fix submit
    
    Delete htmlpreview.min.js
    
    Update readme.md
    
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Update readme.md
    
    try to simplify process with gists
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update readme.md
    
    Update index.html
    
    Update readme.md
    
    fix when no html
    
    try no render mode when no html
    
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Render any link
    
    fix commented scripts
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update index.html
    
    Update index.html
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js
    
    Update htmlpreview.js

--- htmlpreview.js
+++ htmlpreview.js
@@ -8,44 +8,99 @@ var HTMLPreview = {
 		return location.search.substring(1); //Get everything after the ?
 	},
 
+	getGistList: function (yourUrl){
+		    let Httpreq = new XMLHttpRequest(); // a new request
+		    Httpreq.open("GET",yourUrl,false);
+		    Httpreq.send(null);
+		    return JSON.parse(Httpreq.responseText);          
+	},
+	getUrlJson: function (json) {
+		if (json.raw_url) {
+			let arrayUrl = json.raw_url.split("/");
+			let fileName = arrayUrl.pop();
+			arrayUrl.pop();
+			arrayUrl.push(fileName);
+			return location.href.split('?')[0] + '?' + arrayUrl.join("/");
+		} else if (json.download_url) {
+			return location.href.split('?')[0] + '?' + json.download_url;
+		}
+		else {
+			return location.href;
+		}
+	},
+	findFile: function (files) {
+		let filesKeys = Object.keys(files);
+		let firstHtml = false;
+		for(i = 0; i < filesKeys.length; ++i) {
+			let name = files[filesKeys[i]].filename ? files[filesKeys[i]].filename : files[filesKeys[i]].name;
+			if (name.indexOf('index.html') > 0) {
+				location.href = HTMLPreview.getUrlJson(files[filesKeys[i]]);
+				return false;
+			} else if (name.indexOf('.html') > 0 && !firstHtml) {
+				firstHtml = HTMLPreview.getUrlJson(files[filesKeys[i]]);
+			}
+		}
+		if (firstHtml) {
+			location.href = firstHtml;
+			return false;
+		}
+		location.href = HTMLPreview.file() + '?no-render=true';
+		return false;
+	},
+	getGistHtmlFile() {
+		let jsonGist;
+		if (HTMLPreview.file().indexOf('//gist.github.com/') > 0) {
+			jsonGist = HTMLPreview.getGistList('https://api.github.com/gists/' + HTMLPreview.file().split('/').slice(-1)[0]).files;
+			HTMLPreview.findFile(jsonGist);
+		} else if (HTMLPreview.file().indexOf('//github.com/') > 0) {
+			jsonGist = HTMLPreview.getGistList('https://api.github.com/repos/' + HTMLPreview.file().split('/').slice(-2).join('/') + '/contents/');
+			HTMLPreview.findFile(jsonGist);			
+		} else {
+		console.log('Can\'t render this link, than\'s not an html file or git / gist repos');
+		}
+	},
 	raw: function() {
 		return HTMLPreview.file().replace(/\/\/github\.com/, '//raw.githubusercontent.com').replace(/\/blob\//, '/'); //Get URL of the raw file
+		
 	},
 
 	replaceAssets: function() {
-		var frame, a, link, script, i, href, src;
+		let frame, a, link, script, i, href, src;
 		frame = document.querySelectorAll('iframe[src],frame[src]');
 		for(i = 0; i < frame.length; ++i) {
 			src = frame[i].src; //Get absolute URL
-			if(src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-				frame[i].src = '//' + location.hostname + location.pathname + '?' + src; //Then rewrite URL so it can be loaded using YQL
+			if(src.indexOf('//gist.githubusercontent.com') > 0 || src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+				frame[i].src = location.href.split('?')[0] + '?' + src; //Then rewrite URL so it can be loaded using YQL
 			}
 		}
 		a = document.querySelectorAll('a[href]');
 		for(i = 0; i < a.length; ++i) {
 			href = a[i].href; //Get absolute URL
 			if(href.indexOf('#') > 0) { //Check if it's an anchor
-				a[i].href = '//' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
+				a[i].href = location.href.split('?')[0] + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
 			}
-			else if((href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) && (href.indexOf('.html') > 0 || href.indexOf('.htm') > 0)) { //Check if it's from raw.github.com or bitbucket.org and to HTML files
-				a[i].href = '//' + location.hostname + location.pathname + '?' + href; //Then rewrite URL so it can be loaded using YQL
-			}
+			else if((href.indexOf('//gist.githubusercontent.com') > 0 || href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) && (href.indexOf('.html') > 0 || href.indexOf('.htm') > 0)) { //Check if it's from raw.github.com or bitbucket.org and to HTML files
+				a[i].href = location.href.split('?')[0] + '?' + href; //Then rewrite URL so it can be loaded using YQL
+			} 
 		}
 		if(document.querySelectorAll('frameset').length)
 			return; //Don't replace CSS/JS if it's a frameset, because it will be erased by document.write()
 		link = document.querySelectorAll('link[rel=stylesheet]');
 		for(i = 0; i < link.length; ++i) {
 			href = link[i].href; //Get absolute URL
-			if(href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+			if(href.indexOf('//gist.githubusercontent.com') > 0 || href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0 || href.indexOf('//') === -1) { //Check if it's from raw.github.com or bitbucket.org
 				HTMLPreview.send(href, 'loadCSS'); //Then load it using YQL
+			}
+			else if (href.indexOf('//') === -1) {
+				HTMLPreview.send('https://raw.githubusercontent.com/' + href, 'loadCSS');
 			}
 		}
 		script = document.querySelectorAll('script[type="text/htmlpreview"]');
 		for(i = 0; i < script.length; ++i) {
 			src = script[i].src; //Get absolute URL
-			if(src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+			if(src.indexOf('//gist.githubusercontent.com') > 0 || src.indexOf('//raw.githubusercontent.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
 				HTMLPreview.send(src, 'loadJS'); //Then load it using YQL
-			}
+			} 
 			else { //Append all inline scripts
 				script[i].removeAttribute('type');
 				document.write(script[i].outerHTML);
@@ -66,7 +121,7 @@ var HTMLPreview = {
 			&& data.query.results.resources
 			&& data.query.results.resources.content
 			&& data.query.results.resources.status == 200) {
-			HTMLPreview.content = data.query.results.resources.content.replace(/<head>/i, '<head><base href="' + HTMLPreview.raw() + '">').replace(/<script( type=["'](text|application)\/javascript["'])?/gi, '<script type="text/htmlpreview"').replace(/<\/body>/i, '<script src="//' + location.hostname + '/htmlpreview.min.js"></script><script>HTMLPreview.replaceAssets();</script></body>').replace(/<\/head>\s*<frameset/gi, '<script src="//' + location.hostname + '/htmlpreview.min.js"></script><script>document.addEventListener("DOMContentLoaded",HTMLPreview.replaceAssets,false);</script></head><frameset'); //Add <base> just after <head> and inject <script> just before </body> or </head> if <frameset>
+			HTMLPreview.content = data.query.results.resources.content.replace(/<head>/i, '<head><base href="' + HTMLPreview.raw() + '">').replace(/<script( type=["'](text|application)\/javascript["'])?/gi, '<script type="text/htmlpreview"').replace(/<\/body>/i, '<script src="' + location.href.split('?')[0] + '/htmlpreview.js"></script><script>HTMLPreview.replaceAssets();</script></body>').replace(/<\/head>\s*<frameset/gi, '<script src="' + location.href.split('?')[0] + '/htmlpreview.js"></script><script>document.addEventListener("DOMContentLoaded",HTMLPreview.replaceAssets,false);</script></head><frameset'); //Add <base> just after <head> and inject <script> just before </body> or </head> if <frameset>
 			setTimeout(function() {
 				document.open();
 				document.write(HTMLPreview.content);
@@ -121,15 +176,20 @@ var HTMLPreview = {
 	},
 
 	submitform: function() {
-		location.href = '/?' + document.getElementById('file').value;
+		location.href = location.href.split('?')[0] + '?' + document.getElementById('file').value;
 		return false;
 	},
 
 	init: function() {
 		HTMLPreview.previewform.onsubmit = HTMLPreview.submitform;
 		if(HTMLPreview.file()) {
-			HTMLPreview.previewform.innerHTML = '<p>Loading...</p>';
-			HTMLPreview.send(HTMLPreview.raw(), 'loadHTML');
+			if (HTMLPreview.file().indexOf('.html') === -1  && HTMLPreview.file().indexOf('?no-render=true') === -1) {
+				HTMLPreview.getGistHtmlFile(HTMLPreview.file());
+				return false;
+			} else {
+				HTMLPreview.previewform.innerHTML = '<p>Loading...</p>';
+				HTMLPreview.send(HTMLPreview.raw(), 'loadHTML');
+			}
 		}
 	}
 }
